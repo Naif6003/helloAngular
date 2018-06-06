a63738a46b98113e142d733fe25d3bad5b607b98
Naif Said <naifsaid@naifs-mbp.attlocal.net>
2018-06-05 17:55:49 -0700

    disply number of courses in courses component

--- helloAngular/src/app/courses.component.ts
+++ helloAngular/src/app/courses.component.ts
@@ -1,11 +1,23 @@
 import { Component } from "@angular/core";
-
+let courses = require("./service.component")
 
 @Component({
     selector: 'courses',   // <courses> </courses>
-    template: `<h2> Courses Component </h2>`
+    template: ` <b> {{title}} </b>  
+                <br/>
+                <div *ngFor="let course of coursesList">
+                     Course id: {{course.id}} <br/>
+                     course name: {{course.name}} <br/>
+                     course number: {{course.number}} <br/>
+                     course units: {{course.units}} <br/>
+                     course instructor: {{course.instructor}} <br/>
+                     <br/>
+                </div>
+                `
+
 })
 
 export class CoursesComponent {
-    
+    title = "List of Courses: "
+    coursesList = courses
 }
---
+++ helloAngular/src/app/service.component.ts
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        id: 0,
+        name: "Introduction to Algorithms",
+        number: "CS 5112",
+        units: 3,
+        instructor: "Parviz"
+    },
+    {
+        id: 1,
+        name: "Introduction to Data Science",
+        number: "CS 5661",
+        units: 3,
+        instructor: "Mohammed"
+    },
+    {
+        id: 0,
+        name: "Introduction to Web Development",
+        number: "CS 5220",
+        units: 3,
+        instructor: "Sun"
+    }
+]
