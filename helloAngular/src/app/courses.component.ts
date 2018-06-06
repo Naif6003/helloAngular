1c08b5be94369048a6b6e29e83e85df37130816f
Naif Said <naifsaid@naifs-mbp.attlocal.net>
2018-06-05 17:34:48 -0700

    creating component using ng g c course.

---
+++ helloAngular/src/app/courses.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'courses',   // <courses> </courses>
+    template: `<h2> Courses Component </h2>`
+})
+
+export class CoursesComponent {
+    
+}
