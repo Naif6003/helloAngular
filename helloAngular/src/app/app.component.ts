ec51fb12749efc84bf46a47e5bf123f813ca30ee
Naif Said <naifsaid@naifs-mbp.attlocal.net>
2018-06-07 16:36:46 -0700

    ngIf usage

--- helloAngular/src/app/app.component.ts
+++ helloAngular/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FavoriteclickdEventArgs } from './favorite/favorite.component';
 
 @Component({
   selector: 'app-root',
@@ -7,27 +6,7 @@ import { FavoriteclickdEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  post = {
-    title: "Title",
-    isSelected: true,
-    username: "naif"
-  }
 
-  tweet = {
-    body: 'Here is the body of a tweet ...',
-    isLiked: false,
-    likesCount: 0
-  }
+  courses = [1 , 2];
 
-  increment(){
-    this.tweet.isLiked = !this.tweet.isLiked;
-    if(this.tweet.isLiked){
-        this.tweet.likesCount++;
-    }else{
-      this.tweet.likesCount--;
-    }
-  }
-  onFavoriteChange(eventArgs: FavoriteclickdEventArgs){
-    console.log(eventArgs);
-  }
 }
